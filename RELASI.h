9ffcb546065384fc9440689fd768051fdddc8d0d
fahrul31 <59928245+fahrul31@users.noreply.github.com>
2020-04-24 23:30:10 +0700

    update relasi

--- RELASI.h
+++ RELASI.h
@@ -28,6 +28,7 @@ struct listRelasi{
 
 addressRelasi alokasi_Relasi(addressMobil P,addrChild Q);
 void createListRelasi(listRelasi &L);
+void dealokasiRelasi(addressRelasi &P);
 void insertFirstRelasi(listRelasi &L, addressRelasi P);
 void insertLastRelasi(listRelasi &L, addressRelasi P);
 void insertAfterRelasi(addressRelasi Prec, addressRelasi P);
@@ -36,6 +37,9 @@ void deleteLastRelasi(listRelasi &L, addressRelasi &P);
 void deleteAfterRelasi(addressRelasi Prec, addressRelasi &P);
 addressRelasi checkConnection(listRelasi L,ListChild L2,List_mobil L3,int X, int Y);
 void Connection(listRelasi &L,ListChild L2,List_mobil L3,int X, int Y);
+void disconnect(listRelasi &L,ListChild L2,List_mobil L3,int X, int Y);
+void Delete_Child(listRelasi &L,ListChild &L2,int X);
+void Delete_Parent(listRelasi &L,List_mobil &L2,int X);
 
 
 #endif // RELASI_H_INCLUDED
