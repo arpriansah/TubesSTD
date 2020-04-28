4503bc15d8c7f452b4dd440ae52b7dae7af95927
arpriansah <59946822+arpriansah@users.noreply.github.com>
2020-04-29 01:53:55 +0800

    Update Case_Functional.h

--- Case_Functional.h
+++ Case_Functional.h
@@ -1,7 +1,10 @@
 #ifndef CASE_FUNCTIONAL_H_INCLUDED
 #define CASE_FUNCTIONAL_H_INCLUDED
 #include "RELASI.h"
+#include "Mobil.h"
+#include "List_Child.h"
 
+void PengeluaranPelanggan(List_mobil LM, listRelasi LR);
 
 
 #endif // CASE_FUNCTIONAL_H_INCLUDED
